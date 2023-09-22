55de6ec2e6568d9c47a016c9ef8b41bde727ad5b
Rodger <faith.yrj@gmail.com>
2023-09-22 18:07:50 +0800

    1807

--- config.js
+++ config.js
@@ -3,17 +3,16 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "My dear",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Today's your birthday",  // 同上...
+        "And it's been 60 days of us together",
+        "From the 霸王茶姬 we first met",
+        "To the 商朝消火栓 we've become lately,",
+        "We've had endless joy and mishaps",
+        "I hope you stay happy always",
+        "And I hope to be with you forever",
+        "Love you~",
+        "From Rodger",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +24,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": "./imgs/jiangjiang.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "这是什么~",
+        play: "让我康康",
+        bannar_coming: "嘛咪嘛咪哄",
+        balloons_flying: "好像还少点什么呢",
+        cake_fadein: "蛋糕快出来",
+        light_candle: "蜡烛快出来",
+        wish_message: "祝你生日快乐！",
+        story: "有话对你说",
     }
 };
